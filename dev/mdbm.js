91e69221515967223143d0b6e0bae6b0327ed3e7
Domixy500 <95097682+Domixy500@users.noreply.github.com>
2024-11-16 19:06:43 +0100

    Aktualisieren von mdbm.js

--- dev/mdbm.js
+++ dev/mdbm.js
@@ -1,4 +1,14 @@
 const mdbm = (function () {
+  const settings = initSettings();
+
+  function initSettings() {
+    let settingsEntry;
+  }
+
+  function getId() {
+
+  }
+
   function test() {
     message("abc");
   }
