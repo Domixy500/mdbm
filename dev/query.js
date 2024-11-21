33b8a9f17a9fc5c9b89c277dfa6f540f72aa789e
Domixy500 <dominik_kaiser@gmx.net>
2024-11-21 06:14:00 +0100

    update query.js

--- dev/query.js
+++ dev/query.js
@@ -12,13 +12,6 @@ const query = (function () {
             pattern: "SELECT id FROM \"${type}\" WHERE \"mdbm.id\" = \"${id}\""
         }
     };
-    const makeTemplate = R.curry(function (pattern, values) {
-        const regex = new RegExp("\\$\\{(.*?)\\}", "g");
-        return pattern.replace(
-            regex,
-            (match, key) => values[key] || match
-        );
-    });
 
     const make = (template, values) => R.curry(R.pipe(
         values,
@@ -28,10 +21,22 @@ const query = (function () {
     const from = function (definition) {
         const {pattern, values} = definition;
         return make(
-          template(pattern),
+          makeTemplate(pattern),
           values
         );
     };
 
     return R.map(from, definition);
 }());
+
+const makeTemplate = R.curry(function (pattern, values) {
+    const regex = new RegExp("\\$\\{(.*?)\\}", "g");
+    const replacer = function (match, key) {
+      return values[key] || match;
+    };
+    
+    return pattern.replace(
+        regex,
+        replacer
+    );
+});
