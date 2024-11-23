e595976e66e1413c5796f5c8042be7a87ce36932
Domixy500 <dominik_kaiser@gmx.net>
2024-11-23 12:54:07 +0100

    update mdbm.js

--- dev/mdbm.js
+++ dev/mdbm.js
@@ -12,7 +12,7 @@ const mdbm = (function () {
         }
         
         function displayName(e) {
-            return e.field("mdbmId");
+            return e.field("mdbmId") + " - " + e.field("Name");
         }
         
         function displayName2(e) {
