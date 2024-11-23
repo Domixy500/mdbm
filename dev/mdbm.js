388db52c9f5a0451082ea8dbe34544149875a9b8
Domi <Domi@PC2013>
2024-11-23 09:23:23 +0100

    query imported in mdbm

--- dev/mdbm.js
+++ dev/mdbm.js
@@ -1,57 +1,45 @@
 /*jslint*/
 /*global
-    libByName
-    message
     R
-    sql
 */
 
 const mdbm = (function () {
     const query = (function () {
-        const definition = {
-            "byIdType": {
-                parameters: function (id, type) {
+        const definitions = {
+            byIdType: {
+                pattern: "SELECT id FROM \"${type}\" WHERE \"mdbm.id\" = \"${id}\"", //jslint-ignore-line
+                values: function (id, type) {
                     return {"id": id, "type": type};
-                },
-                pattern: "SELECT id FROM \"${type}\""
-                + " WHERE \"mdbm.id\" = \"${id}\""
+                }
             }
         };
 
-        const template = R.curry(function (pattern, values) {
-            const regex = new RegExp("\\$\\{(.*?)\\}", "g");
-            return pattern.replace(
-                regex,
-                (match, key) => values[key] || match
+        const makeQuery = (template, values) => R.curry(R.pipe(
+            values,
+            template
+        ));
+
+        const queryFrom = function (definition) {
+            const {pattern, values} = definition;
+            return makeQuery(
+                template(pattern),
+                values
             );
-        });
+        };
 
-        function makeQuery(queryDefinition) {
-            const {
-                parameters,
-                pattern
-            } = queryDefinition;
-
-            return template(pattern)(parameters);
-        }
-
-        // function makeQuery(queryDefinition) {
-        //     const {
-        //         parameters,
-        //         pattern
-        //     } = queryDefinition;
-        //     const numberOfParameters = parameters.length;
-
-        //     return R.curryN(numberOfParameters, function (...values) {
-        //         return template(
-        //             pattern,
-        //             parameters(...values)
-        //         );
-        //     });
-        // }
-
-        return R.map(makeQuery, definition);
+        return R.map(queryFrom, definitions);
     }());
+    const template = R.curry(function (pattern, values) {
+        const regex = new RegExp("\\$\\{(.*?)\\}", "g");
+        const replacer = function (match, key) {
+            return values[key] || match;
+        };
+
+        return pattern.replace(
+            regex,
+            replacer
+        );
+    });
 
     return Object.freeze({
         "query": query
