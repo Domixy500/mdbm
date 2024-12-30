0ad88e8ead7ea99cc021ca6db4af0d6fa3926a01
Domixy500 <dominik_kaiser@gmx.net>
2024-12-30 07:31:57 +0100

    update mdbm.js

--- dev/mdbm.js
+++ dev/mdbm.js
@@ -7,6 +7,25 @@
 
 const mdbm = (function () {
     
+    function replaceWith(values) {
+        return function(match, key) {
+            return values[key] || match;
+        };
+    }
+    
+    function template(pattern) {
+        const regex = new RegExp("\\$\\{(.*?)\\}", "g");
+
+        return function(values) {
+            const replacer = replaceWith(values);
+    
+            return pattern.replace(
+                regex,
+                replacer
+            );
+        };
+    }
+
     return Object.freeze({
         "object": {}
     });
