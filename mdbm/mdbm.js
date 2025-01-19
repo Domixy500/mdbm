201c9f0f42eb780d0c6204f041fd8b3b98ae7c7c
Domixy500 <dominik_kaiser@gmx.net>
2025-01-19 14:25:48 +0100

    update mdbm.js

--- mdbm/mdbm.js
+++ mdbm/mdbm.js
@@ -10,11 +10,20 @@ function mdbmObject(e) {
         return e.field("mdbmData")[0];
     }
     
+    function eventCreateInit() {
+        e.set("mdbmData", {});
+    }
+    
     function id() {
         return data().id;
     }
     
     return {
+        "event": {
+            "create": {
+                "init": eventCreateInit
+            }
+        },
         "id": id
     };
 }
