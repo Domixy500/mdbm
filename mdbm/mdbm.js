5f5522907c51c6c9dd521ab53bce4b1f67239a55
Domixy500 <dominik_kaiser@gmx.net>
2025-01-19 13:40:36 +0100

    update mdbm.js

--- mdbm/mdbm.js
+++ mdbm/mdbm.js
@@ -1,1 +1,25 @@
-/* jslint*/
+/*jslint*/
+/*global*/
+
+function mdbmCommon() {
+    return {};
+}
+
+function mdbmObject(e) {
+    function data() {
+        return e.field("mdbmData")[0];
+    }
+    
+    function id() {
+        return data().id;
+    }
+    
+    return {
+        "id": id
+    };
+}
+
+const mdbm = {
+    "common": mdbmCommon(),
+    "object": mdbmObject
+};
