ac55b5e6fc9e3c45b6249ce8fbd7362b25255e6d
Domixy500 <dominik_kaiser@gmx.net>
2025-02-04 18:22:57 +0100

    update mdbm.js

--- mdbm/mdbm.js
+++ mdbm/mdbm.js
@@ -2,13 +2,44 @@
 /*global*/
 
 function mdbmCommon() {
+    function errorIfUndefined(msg, variables) {
+        if (variables.some(isUndefined)) {
+            throw new Error(msg);
+        }
+    }
     
+    function isUndefined(variable) {
+        return variable === undefined
+    }
+    
+    return {
+        "errorIfUndefined": errorIfUndefined
+    };
 }
 
 function mdbmObject(e) {
+    const common = mdbmCommon();
+
+    function eventCreateInit(currentLibrary) {
+        common.errorIfUndefined(
+            "eventCreateInit: currentLibrary is not defined",
+            [currentLibrary]
+        );
+        //e.set("mdbmData", [{}]);
+        //e.set(
+        //    "mdbmCurrentLibrary",
+        //    currentLibrary
+        //);
+    }
     
     return {
-        
+        "event": {
+            "create": {
+                "before": "eventCreateBefore",
+                "init": eventCreateInit
+            }
+        },
+        "id": "id"
     };
 }
 
