79e39c1109a0e71353b823deefa7d2b4e45f8379
Domixy500 <dominik_kaiser@gmx.net>
2025-02-05 06:23:50 +0100

    update mdbm.js

--- mdbm/mdbm.js
+++ mdbm/mdbm.js
@@ -12,19 +12,64 @@ function mdbmCommon() {
         return variable === undefined
     }
     
+    function jsonParse(str) {
+        return JSON.parse(
+            replaceAll(
+                str,
+                "'",
+                "\""
+            )
+        );
+    }
+    
+    function jsonStringify(obj) {
+        return replaceAll(
+            JSON.stringify(
+                obj,
+                null,
+                2
+            ),
+            "\"",
+            "'"
+        );
+    }
+    
+    function replaceAll(str, searchValue, replaceValue) {
+        return str.replace(
+            new RegExp(searchValue, "g"),
+            replaceValue
+        );
+    }
+    
     return {
-        "errorIfUndefined": errorIfUndefined
+        "errorIfUndefined": errorIfUndefined,
+        "isUndefined": isUndefined,
+        "json": {
+            "parse": jsonParse,
+            "stringify" jsonStringify
+            
+        }
     };
 }
 
 function mdbmObject(e) {
     const common = mdbmCommon();
+    
+    function data(key, value) {
+        const paras = [key, value];
+        if(paras.some(common.isUndefined)) {
+            return common.json.parse(
+                e.field("mdbmData")
+            );
+        }
+    }
 
     function eventCreateInit(currentLibrary) {
         common.errorIfUndefined(
             "eventCreateInit: currentLibrary is not defined",
             [currentLibrary]
         );
+        log(common.json.stringify(data()))
         //e.set("mdbmData", [{}]);
         //e.set(
         //    "mdbmCurrentLibrary",
