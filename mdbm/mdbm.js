05059142800be7a2be36a46f9bc95df595fcbded
Domixy500 <95097682+Domixy500@users.noreply.github.com>
2025-02-08 10:22:17 +0100

    createBefore

--- mdbm/mdbm.js
+++ mdbm/mdbm.js
@@ -1,5 +1,7 @@
 /*jslint*/
-/*global*/
+/*global
+    R
+*/
 
 function mdbmCommon() {
     function errorIfUndefined(msg, variables) {
@@ -21,8 +23,48 @@ function mdbmCommon() {
 function mdbmObject(e) {
     const common = mdbmCommon();
 
-    function eventCreateBefore() {
+    function data(key, value) {
+        const paras = [key, value];
+        if (paras.some(common.isUndefined)) {
+            return common.json.parse(
+                e.field("mdbmData")
+            );
+        } else {
+            const mdbmData = data();
+            mdbmData[key] = value;
+            e.set(
+                "mdbmData",
+                common.json.stringify(
+                    mdbmData
+                )
+            );
+            return mdbmData;
+        }
+    }
 
+    function entryIds() {
+        const stored = data().entryIds;
+        const ids = (
+            stored === undefined
+            ? entryIdsNew()
+            : JSON.parse(stored)
+        );
+        data(
+            "entryIds",
+            JSON.stringify(ids)
+        );
+        return ids;
+    }
+
+    function entryIdsNew() {
+        const ids = R.fromPairs(
+            libraries().map((x) => [x, null])
+        );
+        return ids;
+    }
+
+    function eventCreateBefore() {
+        entryIds();
     }
 
     function eventCreateInit(currentLibrary) {
@@ -30,10 +72,15 @@ function mdbmObject(e) {
             "eventCreateInit: currentLibrary is not defined",
             [currentLibrary]
         );
+        e.set("mdbmData", [{}]);
         e.set(
             "mdbmCurrentLibrary",
             currentLibrary
         );
+    }
+
+    function libraries() {
+        return data().libraries.split(",");
     }
 
     return {
