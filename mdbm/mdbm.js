76ae9609914afb89b91a151bcb90e50018e2361e
Domixy500 <95097682+Domixy500@users.noreply.github.com>
2025-02-08 10:05:12 +0100

    eventCreateInit

--- mdbm/mdbm.js
+++ mdbm/mdbm.js
@@ -7,6 +7,31 @@ function mdbmCommon() {
 
 function mdbmObject(e) {
     const common = mdbmCommon();
+
+    function eventCreateBefore() {
+
+    }
+
+    function eventCreateInit(currentLibrary) {
+        common.errorIfUndefined(
+            "eventCreateInit: currentLibrary is not defined",
+            [currentLibrary]
+        );
+        e.set(
+            "mdbmCurrentLibrary",
+            currentLibrary
+        );
+    }
+
+    return {
+        "event": {
+            "create": {
+                "before": eventCreateBefore,
+                "init": eventCreateInit
+            }
+        },
+        "id": "id"
+    };
 }
 
 function mdbm() {
