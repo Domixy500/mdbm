2b1a6b9eed42d24eaf2d66fa4af0b31d4caa2f14
Domixy500 <95097682+Domixy500@users.noreply.github.com>
2025-02-15 22:58:37 +0100

    mdbmData

--- mdbm/mdbm.js
+++ mdbm/mdbm.js
@@ -5,6 +5,38 @@
 
 "use strict";
 
+const _mdbmData = (function () {
+
+    function entryIds(e) {
+        const data = mdbmData(e);
+
+        return function(newValue) {
+            return (
+                newValue === undefined
+                ? data().entryIds
+                : data({"entryIds": newValue})
+            );
+        };
+    }
+
+    function fromEntry(e) {
+
+
+        return Object.freeze({
+            "entryIds": entryIds(e)
+        });
+    }
+
+    function mdbmData(e) {
+        return _mdbmField.fromName("mdbmData", e);
+    }
+
+    return Object.freeze({
+        "fromEntry": fromEntry
+    });
+}());
+
+
 const _mdbmField = (function () {
     const field = R.curry(function (e, fieldName) {
         return function (newValue) {
@@ -30,18 +62,50 @@ const _mdbmField = (function () {
 }());
 
 const _mdbmObject = (function () {
-    const getFields = _mdbmField.fromNames([
+    const fields = _mdbmField.fromNames([
         "mdbmCurrentLibrary",
         "mdbmData"
     ]);
 
+    // const initObject = R.curry(function (e, libraryName) {
+    //     mdbmDataInit(e);
+    //     setData(
+    //         e,
+    //         "mdbmCurrentLibrary",
+    //         libraryName
+    //     );
+    //     return e;
+    // });
+
+
+
     function fromEntry(e) {
-        const fields = getFields(e);
+        const {
+            mdbmCurrentLibrary,
+            mdbmData
+        } = fields(e);
 
         return Object.freeze({
-            mdbmCurrentLibrary: fields.mdbmCurrentLibrary,
-            mdbmData: fields.mdbmData
+            "mdbmCurrentLibrary": mdbmCurrentLibrary,
+            "mdbmData": mdbmData
         });
+    }
+
+    function init(e) {
+
+        return function (libraryName) {
+
+        };
+    }
+
+    function mdbmData(e) {
+        return function (newValue) {
+            return (
+                newValue === undefined
+                ? fields(e).mdbmData()[0]
+                : e.set("mdbmData", newValue)
+            );
+        };
     }
 
     return Object.freeze({
