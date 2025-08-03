3a113fdec4997b2629d5d2ed6667fa5a905d4cb4
Domixy500 <dominik_kaiser@gmx.net>
2025-08-03 14:09:05 +0200

    update index.js

--- src/mdbm/index.js
+++ src/mdbm/index.js
@@ -2,9 +2,9 @@
 /*global*/
 
 import {notify} from "../app";
-import {valueAsString} from "./property";
+import {property} from "./property";
 
 export {
     notify,
-    valueAsString
+    property
 };
