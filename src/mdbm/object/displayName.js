a223bf024be738a4e2f9b7e20c1d83419a244363
Domixy500 <dominik_kaiser@gmx.net>
2025-08-08 06:01:14 +0200

    update displayName.js

--- src/mdbm/object/displayName.js
+++ src/mdbm/object/displayName.js
@@ -1,12 +1,29 @@
+function patternFromObject(e) {
+    return e.field("DisplayNamePattern") || patternFromPrototype(
+            e.field("Prototype")[0]
+    );
+}
+
+
+function patternFromPrototype(e) {
+    return e.field("DisplayNamePattern") || patternFromPrototype(
+            e.field("basedOn")[0]
+    );
+}
+
 function displayName(e) {
-    const pattern = e.field("DisplayNamePattern");
+    const pattern = patternFromObject(e);
     const properties = e.linksFrom("Property", "Object");
     
     function replacer(_, fieldName) {
         const property = properties.filter(
             (x) => x.field("Label") === fieldName
         )[0];
-        return property.field("Value");
+        return (
+            property
+            ? property.field("Value")
+            : ""
+        );
     }
 
     return pattern.replace(
