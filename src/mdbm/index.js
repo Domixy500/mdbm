25c8038023507c64a93bf03ffaf93f95a2e4d7c1
Domixy500 <dominik_kaiser@gmx.net>
2025-08-09 17:43:30 +0200

    update index.js

--- src/mdbm/index.js
+++ src/mdbm/index.js
@@ -1,12 +1,8 @@
 /*jslint beta*/
 /*global*/
 
-import {notify} from "@app";
 import {object} from "./object";
-import {property} from "./property";
 
 export {
-    notify,
-    object,
-    property
+    object
 };
