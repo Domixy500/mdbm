2e0d3f28044b752f38c4203a0a16fd6caf4e5e55
Domixy500 <dominik_kaiser@gmx.net>
2025-10-16 10:12:26 +0200

    update addType.js

--- src/type/addType.js
+++ src/type/addType.js
@@ -6,7 +6,7 @@ import {find} from "./find";
 function addType(typeEntry, typeName) {
     const hasTypes = typeEntry.field("hasTypes");
     const hasTypeNames = hasTypes.map(
-        (x) => e.field("Name")
+        (e) => e.field("Name")
     );
     if (hasTypeNames.includes(typeName)) {
         typeEntry.link(
