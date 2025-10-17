4730223e11b825e5d18d5a5ce49274548a2661ff
Domixy500 <dominik_kaiser@gmx.net>
2025-10-17 16:39:14 +0200

    update messages.js

--- src/type/messages.js
+++ src/type/messages.js
@@ -2,8 +2,13 @@
 /*global*/
 
 const messages = {
+    alreadyExists,
     isMissing
 };
+
+function alreadyExists(x) {
+    return "Type '" + x + "' does already exist!";
+}
 
 function isMissing(x) {
     return "Type '" + x + "' does not exist!";
