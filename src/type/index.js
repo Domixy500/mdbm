3047b32a9fa1ef50278b721a734beff28532ac79
Domixy500 <dominik_kaiser@gmx.net>
2025-10-20 19:58:38 +0200

    update index.js

--- src/type/index.js
+++ src/type/index.js
@@ -6,6 +6,7 @@ import {create} from "./create";
 import {entries} from "./entries";
 import {exists} from "./exists";
 import {find} from "./find";
+import {idObject} from "./idObjects";
 import {isMissing} from "./isMissing";
 import {messages} from "./messages.js";
 import {onCreate} from "./onCreate.js";
@@ -13,6 +14,7 @@ import {onCreate} from "./onCreate.js";
 const type = {
     check,
     create,
+    idObject,
     isMissing,
     messages,
     onCreate
