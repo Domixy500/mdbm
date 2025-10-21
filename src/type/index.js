059a4b12cb57d0ca36eb2870cb2bd2644363e73b
Domixy500 <dominik_kaiser@gmx.net>
2025-10-21 21:33:45 +0200

    fromObjectEntry

--- src/type/index.js
+++ src/type/index.js
@@ -7,6 +7,7 @@ import {entries} from "./entries";
 import {exists} from "./exists";
 import {find} from "./find";
 import {emptyIds} from "./emptyIds";
+import {fromObjectEntry} from "./fromObjectEntry";
 import {isMissing} from "./isMissing";
 import {messages} from "./messages.js";
 import {onCreate} from "./onCreate.js";
@@ -15,6 +16,7 @@ const type = {
     check,
     create,
     emptyIds,
+    fromObjectEntry,
     isMissing,
     messages,
     onCreate
