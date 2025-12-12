b11e1af80d90d396cec14cc6c4717ccbf302b439
domixy500 <dominik_kaiser@gmx.net>
2025-12-12 17:31:03 +0100

    update

--- src/type/fromEntry.ts
+++ src/type/fromEntry.ts
@@ -1,7 +1,9 @@
+import {id} from "./id";
 import {name} from "./name";
 
 export function fromEntry(typeEntry: Entry): Type {
     return Object.freeze({
+        id: () => id(typeEntry),
         name: () => name(typeEntry)
     });
 }
---
+++ src/type/id.ts
@@ -0,0 +1,3 @@
+export function id(typeEntry: Entry): string {
+    return typeEntry.id;
+}
