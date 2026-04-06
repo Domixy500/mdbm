f4c3803e675eb034d0dc104ee0cc8f6b9597852e
Domixy500 <dominik_kaiser@gmx.net>
2026-04-06 10:40:50 +0200

    setup without special eslint config

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+/*jslint beta, node*/
+/*global*/
+
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+
+const config = {
+    input: "src/index.ts",
+    output: [
+        {
+            file: "dist/mdbm.debug.js",
+            format: "iife",
+            name: "mdbm",
+            plugins: [
+                terser({
+                    compress: false,
+                    format: {
+                        beautify: true,
+                        comments: false
+                    },
+                    mangle: false
+                })
+            ]
+        },
+        {
+            file: "dist/mdbm.min.js",
+            format: "iife",
+            name: "mdbm",
+            plugins: [
+                terser({
+                    format: {
+                        comments: false
+                    }//, test if can be removed
+                    // mangle: {
+                    //     properties: false
+                    // }
+                })
+            ]
+        }
+    ],
+    plugins: [
+        resolve({
+            extensions: [".ts", ".js"]
+        }),
+        typescript({
+            tsconfig: "./tsconfig.json"
+        })
+    ]
+};
+
+export default config; //jslint-ignore-line
