be2ae3286bc7db628680609ebf5a5b1ce746bd39
Domixy500 <dominik_kaiser@gmx.net>
2026-04-13 07:18:27 +0200

    plugins

--- eslint/plugins.js
+++ eslint/plugins.js
@@ -1,12 +1,12 @@
 /*jslint beta*/
 /*global*/
 
-import eslintPluginTs from "@typescript-eslint/eslint-plugin";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import stylistic from "@stylistic/eslint-plugin";
 import custom from "./custom/index.js";
 
 export default Object.freeze({
     "@custom": custom,
     "@stylistic": stylistic,
-    "@typescript-eslint": eslintPluginTs
+    "@typescript-eslint": typescriptEslint
 });
