15061221a7b386506cc8fb5831e0bba81f3a0592
Domixy500 <dominik_kaiser@gmx.net>
2026-04-14 19:07:52 +0200

    debug logs

--- src/mementoDatabase/globalFunctions.d.ts
+++ src/mementoDatabase/globalFunctions.d.ts
@@ -1,4 +1,6 @@
 /** Get library by name */
 declare function libByName(name: string): Library;
+/** Logs message in Memento Database app */
+declare function log(text: string): void;
 /** Shows a toast message in Memento Database app */
 declare function message(text: string): void;
--- src/object/create.ts
+++ src/object/create.ts
@@ -3,7 +3,9 @@ import objectType from "../objectType";
 
 function fromEntry(baseEntry: Entry, typeName: string): void {
     const ids = objectType.emptyIds(typeName);
+    log(ids.toString());
     ids[typeName] = baseEntry.id;
+    log(JSON.stringify(ids, null, 2));
     Object.keys(ids).forEach(
         function (key: string) {
             if(ids[key] !== "") {
@@ -11,6 +13,7 @@ function fromEntry(baseEntry: Entry, typeName: string): void {
             }
         }
     );
+    log(JSON.stringify(ids, null, 2));
 }
     
 export {
