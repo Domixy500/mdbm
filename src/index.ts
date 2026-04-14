2acd38f1260a61b17981830837c892d11daaa161
Domixy500 <dominik_kaiser@gmx.net>
2026-04-14 18:57:27 +0200

    create object from Entry

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import object from "./object";
 
-function toast(text: string): void {
-    message(text);
-}
-
 export default Object.freeze({
-    object,
-    toast
+    object
 });
--- src/object/create.ts
+++ src/object/create.ts
@@ -1,9 +1,16 @@
+import {createEntry} from "../mementoDatabase";
 import objectType from "../objectType";
 
 function fromEntry(baseEntry: Entry, typeName: string): void {
     const ids = objectType.emptyIds(typeName);
     ids[typeName] = baseEntry.id;
-    
+    Object.keys(ids).forEach(
+        function (key: string) {
+            if(ids[key] !== "") {
+                ids[key] = createEntry(key).id;
+            }
+        }
+    );
 }
     
 export {
