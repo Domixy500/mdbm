4a1e8668ef8e0910dc2899958a2136162ccbcb45
domixy500 <dominik_kaiser@gmx.net>
2026-04-15 18:09:17 +0200

    update index.ts

--- src/object/index.ts
+++ src/object/index.ts
@@ -1,5 +1,30 @@
-import * as create from "./create";
+//import * as create from "./create";
+function defaultIds(e: Entry) {
+    const typeName = mdbmField(e, "Type");
+}
+
+function typeName(e: Entry) {
+    const data = mdbmField("Type");
+    if (data === "") {
+        throw `No Type defined for ${e.id}!`;
+    }
+    
+    return data;
+}
+
+function mdbmField(e: Entry, fieldName:string) {
+    return e.field("mdbm." + fieldName);
+}
+
+function ids(e: Entry): stringDict {
+    const data = mdbmField("Ids");
+    return (
+        data === ""
+        ? defaultIds(e)
+        : JSON.parse(data)
+    );
+}
 
 export default Object.freeze({
-    create
+    ids: getIds
 });
